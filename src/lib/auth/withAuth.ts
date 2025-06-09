c55cbd4acb7cdcc8d52e1432208ec9517f906144
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-09 20:22:07 +0200

    Auth wrapper

---
+++ src/lib/auth/withAuth.ts
@@ -0,0 +1,25 @@
+import { adminAuth } from './firebase-admin';
+import { NextRequest } from 'next/server';
+
+export async function withAuth(
+  req: NextRequest,
+  handler: (uid: string) => Promise<Response>
+): Promise<Response> {
+  const authHeader = req.headers.get('authorization');
+  if (!authHeader) {
+    return new Response('Unauthorized: Missing authorization header', { status: 401 });
+  }
+
+  const token = authHeader?.split('Bearer ')[1];
+  if (!token) {
+    return new Response('Unauthorized: Missing token', { status: 401 });
+  }
+
+  try {
+    const decoded = await adminAuth.verifyIdToken(token);
+    return handler(decoded.uid);
+  } 
+  catch (err) {
+    return new Response('Unauthorized: Invalid token', { status: 401 });
+  }
+}
