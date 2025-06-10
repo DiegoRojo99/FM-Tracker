a0e155e44b37df57a61aedc51c604080b400fd58
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-10 16:08:42 +0200

    Display league and team icons

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { Save } from '@/lib/types/RetrieveDB'
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { useAuth } from '../components/AuthProvider';
+import { SaveWithDetails } from '@/lib/types/Save';
+import Image from 'next/image';
 
 export default function SavesPage() {
   const { user } = useAuth();
   
-  const [saves, setSaves] = useState<Save[]>([]);
+  const [saves, setSaves] = useState<SaveWithDetails[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -37,11 +38,24 @@ export default function SavesPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Your Saves</h1>
-      <div className="grid gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {saves.map(save => (
           <Link key={save.id} href={`/saves/${save.id}`}>
-            <div key={save.id} className="p-4 rounded-xl border hover:shadow transition-all">
-              <p>Team ID: {save.startingTeamId}, League ID: {save.leagueId}, Country Code: {save.countryCode}</p>
+            <div className="p-4 rounded-xl border hover:shadow transition-all w-full flex flex-col items-center gap-4 
+            cursor-pointer hover:border-purple-600 hover:bg-purple-50 hover:dark:bg-purple-900">
+              <Image
+                src={save.team?.logo}
+                alt={save.team?.name}
+                width={128}
+                height={128}
+              />
+              <h2 className="text-xl font-semibold">{save.team?.name}</h2>
+              <Image
+                src={save.league?.logo}
+                alt={save.league?.name}
+                width={128}
+                height={128}
+              />
             </div>
           </Link>
         ))}
