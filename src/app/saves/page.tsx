083eb25a14ec079d49e48d5355c165c54eb3e4f8
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-10 16:40:58 +0200

    Create new save button added to saves page

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -37,7 +37,12 @@ export default function SavesPage() {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Your Saves</h1>
+      <div className='mb-6 flex flex-row items-center justify-between'>
+        <h1 className="text-2xl font-bold mb-4">Your Saves</h1>
+        <Link href="/add-save" className="inline-block mb-4 px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition">
+          Create New Save
+        </Link>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {saves.map(save => (
           <Link key={save.id} href={`/saves/${save.id}`}>
