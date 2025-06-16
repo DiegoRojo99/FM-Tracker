987a0e1ea58d30ba683db31c8654e46742b013fd
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-16 12:59:53 +0200

    Countries based on FM check

--- src/app/api/countries/route.ts
+++ src/app/api/countries/route.ts
@@ -1,9 +1,10 @@
 import { db } from '@/lib/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const snapshot = await getDocs(collection(db, 'countries'));
+  const q = query(collection(db, 'countries'), where('inFootballManager', '==', true));
+  const snapshot = await getDocs(q);
   const data = snapshot.docs.map(doc => doc.data());
   return NextResponse.json(data);
 }
