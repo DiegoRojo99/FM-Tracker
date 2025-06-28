799145cc71b6ec2520593014aef5e3f10fe6905e
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-28 12:29:58 +0200

    Removed unused endpoints

--- src/app/api/competitions/GetLeagueData.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/firebase";
-import { doc, getDoc } from "firebase/firestore";
-
-export async function GetLeagueData(countryCode: string, leagueId: string) {
-  const leagueRef = doc(db, 'countries', countryCode, 'competitions', leagueId);
-  const leagueSnap = await getDoc(leagueRef);
-  const league = leagueSnap.exists() ? { id: leagueSnap.id, ...leagueSnap.data() } : null;
-  return league;
-}
