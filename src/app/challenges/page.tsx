afb05ec3dbff6f29d31aeab7fef00cc95ae24c2a
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-29 22:47:16 +0200

    Updated challenges UI

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -4,6 +4,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/db/firebase";
 import { Challenge } from "@/lib/types/Challenge";
 import FootballLoader from "../components/FootBallLoader";
+import BlurredCard from "../components/BlurredCard";
 
 export default function ChallengesPage() {
   const [challenges, setChallenges] = useState<Challenge[]>([]);
@@ -26,21 +27,34 @@ export default function ChallengesPage() {
   return (
     <div className="p-6 max-w-3xl mx-auto space-y-6">
       <h1 className="text-2xl font-bold m-4 text-white">Challenges</h1>
-      <div className="mb-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {challenges.map(challenge => (
-          <div key={challenge.id} className="flex flex-col p-4 mb-4 bg-white rounded-lg shadow">
-            <h2 className="text-xl font-bold text-black">{challenge.name}</h2>
-            <p className="text-gray-700">{challenge.description}</p>
-            {challenge.bonus && <p className="text-green-600">Bonus: {challenge.bonus}</p>}
-            <h3 className="mt-2 font-semibold text-gray-800">Goals:</h3>
-            <ul className="list-disc pl-5">
-              {challenge.goals.map((goal, index) => (
-                <li key={index} className="text-gray-600">{goal.description}</li>
-              ))}
-            </ul>
+          <div key={challenge.id}>
+            <GlobalChallengeCard challenge={challenge} />
           </div>
         ))}
       </div>
     </div>
   );
 }
+
+const GlobalChallengeCard: React.FC<{ challenge: Challenge }> = ({ challenge }) => {  
+  return (
+    <BlurredCard className="h-full">
+      <h3 className="text-lg font-semibold text-center">{challenge.name}</h3>
+      <p className="text-sm text-gray-400 my-2">{challenge.description}</p>
+      {challenge.bonus && (
+        <p className="text-sm text-yellow-500 my-2">Bonus: {challenge.bonus}</p>
+      )}
+      <div className="mt-2">
+        {challenge.goals.map((goal) => (
+          <div key={goal.id} className="flex items-center justify-between text-sm text-gray-700 dark:text-gray-300 my-1">
+            <span className={`flex-1`}>
+              {goal.description}
+            </span>
+          </div>
+        ))}
+      </div>
+    </BlurredCard>
+  );
+};
