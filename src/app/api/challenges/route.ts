3131cd3881652665fd4570fbae335d3539350bc4
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-29 23:16:28 +0200

    Remove request

--- src/app/api/challenges/route.ts
+++ src/app/api/challenges/route.ts
@@ -1,7 +1,6 @@
-import { NextRequest } from 'next/server';
 import { getAllChallenges } from '@/lib/db/challenges';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const allChallenges = await getAllChallenges();
   return new Response(JSON.stringify(allChallenges), { status: 200 });
 }
