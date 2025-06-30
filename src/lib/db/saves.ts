47af87cf3ab0be3ea9c375f3d232847cfa426a00
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-30 20:08:35 +0200

    Updated db and firestore library

--- src/lib/db/saves.ts
+++ src/lib/db/saves.ts
@@ -1,8 +1,9 @@
-import { doc, updateDoc } from "firebase/firestore";
-import { db } from "./firebase";
+import { doc, getDoc, getDocs, updateDoc, collection } from "firebase/firestore";
+import { db } from '@/lib/db/firebase';
 import { fetchCompetition } from "./competitions";
 import { Competition } from "../types/Country&Competition";
-import { SaveLeague } from "../types/Save";
+import { Save, SaveLeague } from "../types/Save";
+import { CareerStint } from "../types/Career";
 
 /**
  * Updates the 'season' attribute of a save document for a user in Firestore.
@@ -40,4 +41,35 @@ export async function updateSaveCurrentLeague(
     currentLeague: currentLeagueData
   });
   return true;
+}
+
+export async function getSaveById(userId: string, saveId: string) {
+  const saveRef = doc(db, `users/${userId}/saves/${saveId}`);
+  const saveSnapshot = await getDoc(saveRef);
+  console.log("Fetched save:", saveSnapshot.data());
+  if (!saveSnapshot.exists()) return null;
+  return { ...saveSnapshot.data(), id: saveSnapshot.id } as Save;
+}
+
+export async function getSaveWithCareer(userId: string, saveId: string) {
+  const save = await getSaveById(userId, saveId);
+  if (!save) return null;
+
+  const careerRef = collection(db, `users/${userId}/saves/${saveId}/career`);
+  const careerSnapshot = await getDocs(careerRef);
+  const career = careerSnapshot.docs.map(doc => ({ ...doc.data() as CareerStint })) as CareerStint[];
+
+  return { ...save, career };
+}
+
+export async function getUserSaves(userId: string) {
+  const savesRef = collection(db, `users/${userId}/saves`);
+  const savesSnapshot = await getDocs(savesRef);
+  return savesSnapshot.docs.map(doc => ({ ...doc.data() as Save, id: doc.id })) as Save[];
+}
+
+export async function getUserSavesWithCareer(userId: string) {
+  const saves = await getUserSaves(userId);
+  const savesWithCareer = await Promise.all(saves.map(async save => await getSaveWithCareer(userId, save.id)));
+  return savesWithCareer.filter(save => save !== null);
 }
