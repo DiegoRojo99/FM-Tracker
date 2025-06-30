118566921c53031a657b406773f00bb64705d21e
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-30 16:54:54 +0200

    Team type as return

--- src/app/api/teams/[id]/GetTeamData.ts
+++ src/app/api/teams/[id]/GetTeamData.ts
@@ -1,9 +1,10 @@
 import { db } from "@/lib/db/firebase";
+import { Team } from "@/lib/types/Team";
 import { doc, getDoc } from "firebase/firestore";
 
 export async function GetTeamData(teamId: string) {
   const teamRef = doc(db, 'teams', teamId);
   const teamSnap = await getDoc(teamRef);
-  const team = teamSnap.exists() ? { id: teamSnap.id, ...teamSnap.data() } : null;
+  const team = teamSnap.exists() ? { ...teamSnap.data() as Team } : null;
   return team;
 }
