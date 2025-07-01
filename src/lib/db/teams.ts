6f9ddd6d406d1a77d6cb5b963d3d7f6ec738d763
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-01 11:27:19 +0200

    get all teams util

--- src/lib/db/teams.ts
+++ src/lib/db/teams.ts
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { Team } from "../types/Team";
 import { db } from "./firebase";
 
@@ -17,4 +17,17 @@ export async function fetchTeam(teamId: string) {
 export async function getTeamsByIds(ids: string[]) {
   const teams = await Promise.all(ids.map(id => fetchTeam(id)));
   return teams.filter(Boolean) as Team[];
+}
+
+export async function getAllTeams() {
+  const teamsRef = collection(db, "teams");
+  const snapshot = await getDocs(teamsRef);
+  const teams: Team[] = [];
+
+  snapshot.forEach(doc => {
+    const teamData = doc.data() as Team;
+    teams.push(teamData);
+  });
+
+  return teams;
 }
