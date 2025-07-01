239cd0d9ea6ee9897affbc85c0535fc9d5fc5306
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-01 12:32:37 +0200

    Admin leagues page

---
+++ src/app/admin/leagues/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { Competition, Country } from "@/lib/types/Country&Competition";
+import { SaveTeam } from "@/lib/types/Save";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function TeamsPage() {
+  const [leaguesPulled, setLeaguesPulled] = useState<Competition[]>([]);
+  const [leaguesNotPulled, setLeaguesNotPulled] = useState<Competition[]>([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const response = await fetch("/api/admin/leagues");
+      const {
+        countries,
+        leaguesPulled,
+        leaguesNotPulled,
+        teams,
+      } = await response.json();
+      
+      setLeaguesPulled(leaguesPulled);
+      setLeaguesNotPulled(leaguesNotPulled);
+    };
+    fetchTeams();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      {/* Pulled */}
+      <h1 className="text-2xl font-bold mb-4 text-center">Competitions Pulled</h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
+        {leaguesPulled.map((league) => (
+          <div key={league.id} className="flex flex-col items-center p-4 rounded-lg shadow bg-purple-500">
+            <Image
+              src={league.logo}
+              alt={`${league.name} logo`}
+              width={100}
+              height={100}
+              className="mb-2 flex-1 object-contain"
+              unoptimized
+            />
+            <h2 className="text-l font-semibold h-fit pt-2">{league.name}</h2>
+          </div>
+        ))}
+      </div>
+
+      {/* Not Pulled */}
+      <h1 className="text-2xl font-bold mb-4 text-center mt-8">Competitions Not Pulled</h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
+        {leaguesNotPulled.map((league) => (
+          <div key={league.id} className="flex flex-col items-center p-4 rounded-lg shadow bg-purple-500">
+            <Image
+              src={league.logo}
+              alt={`${league.name} logo`}
+              width={100}
+              height={100}
+              className="mb-2 flex-1 object-contain"
+              unoptimized
+            />
+            <h2 className="text-l font-semibold h-fit pt-2">{league.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
