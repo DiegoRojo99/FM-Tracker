a2b2deba3f1a1fe1f5ac8fca5c544aeabc71c1e0
DiegoRojo99 <diego99rojo@gmail.com>
2026-02-01 14:22:21 +0100

    Migrated user type

--- src/lib/db/users.ts
+++ src/lib/db/users.ts
@@ -1,39 +1,22 @@
-import { collection, doc, getDoc } from "firebase/firestore";
-import { db } from '@/lib/db/firebase';
-import { User } from "../types/User";
-import { getDocs } from "firebase/firestore";
+import { User } from "../types/prisma/User";
+import { prisma } from "./prisma";
 
-export function getUserById(userId: string) {
-  try {
-    const userRef = doc(db, 'users', userId);
-    return getDoc(userRef).then(doc => {
-      if (!doc.exists) {
-        throw new Error("User not found");
-      }
-      return {...doc.data() } as User;
-    });
-  }
-  catch (error) {
-    console.error("Error fetching user by ID:", error);
-    throw error;
-  }
+export function getUserById(userId: string): Promise<User | null> {
+  return prisma.user.findUnique({
+    where: {
+      uid: userId,
+    },
+  });
 }
 
-export function getUsersByIds(userIds: string[]) {
-  const userRefs = userIds.map(id => doc(db, 'users', id));
-  return Promise.all(userRefs.map(ref => getDoc(ref)))
-    .then(docs => {
-      return docs.map(doc => {
-        if (!doc.exists) {
-          throw new Error(`User with ID ${doc.id} not found`);
-        }
-        return { ...doc.data() } as User;
-      });
-    });
+export function getUsersByIds(userIds: string[]): Promise<User[]> {
+  return prisma.user.findMany({
+    where: {
+      uid: { in: userIds },
+    },
+  });
 }
 
 export async function getAllUsers(): Promise<User[]> {
-  const collectionRef = collection(db, 'users');
-  const users = await getDocs(collectionRef);
-  return users.docs.map(doc => ({ ...doc.data() })) as User[];
+  return prisma.user.findMany();
 }
---
+++ src/lib/types/prisma/User.d.ts
@@ -0,0 +1,3 @@
+import { User as PrismaUser } from "../../../../prisma/generated/client";
+
+export type User = PrismaUser;
